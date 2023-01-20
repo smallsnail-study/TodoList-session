3c4a299701279aa21faa3b4339f4929181b21045
yunhuiGwak <kwakyh17@naver.com>
2023-01-20 20:21:39 +0900

    기능추가: 실제 로그인을 POST방식으로 처리하는 기능 추가(로그인이 처리된 후에는 list목록페이지로 redirect)

--- src/main/java/org/zerock/w2/controller/LoginController.java
+++ src/main/java/org/zerock/w2/controller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/login")
@@ -18,5 +19,23 @@ public class LoginController extends HttpServlet {      // 로그인처리기능
         log.info("loign get.............");
 
         req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req,resp);
+    }
+
+    @Override   // 실제 로그인처리(POST)
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException {
+
+        log.info("login post...........");
+
+        // 사용자의 mid와 mpw를 파라미터로 수집한다.
+        String mid = req.getParameter("mid");
+        String mpw = req.getParameter("mpw");
+
+        String str = mid+mpw;   // 수집한 mid와 mpw를 이용해서 문자열을 구성한다.
+
+        HttpSession session = req.getSession();
+
+        session.setAttribute("loginInfo",str);  // 구성한 문자열을 HttpSession의 loginInfo이름을 이용해서 저장한다.
+
+        resp.sendRedirect("todo/list"); // 로그인이 처리된 후에는 목록페이지로 리다이렉트
     }
 }

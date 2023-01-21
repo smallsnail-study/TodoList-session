5470197e71e87a1da203a49f4fd260c9010a277c
yunhuiGwak <kwakyh17@naver.com>
2023-01-21 18:17:36 +0900

    기능수정: 정상적으로 로그인된 경우와 예외발생의 경우 분리해서 처리

--- src/main/java/org/zerock/w2/controller/LoginController.java
+++ src/main/java/org/zerock/w2/controller/LoginController.java
@@ -1,6 +1,8 @@
 package org.zerock.w2.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.zerock.w2.dto.MemberDTO;
+import org.zerock.w2.service.MemberService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,12 +32,22 @@ public class LoginController extends HttpServlet {      // 로그인처리기능
         String mid = req.getParameter("mid");
         String mpw = req.getParameter("mpw");
 
+        /*
         String str = mid+mpw;   // 수집한 mid와 mpw를 이용해서 문자열을 구성한다.
 
         HttpSession session = req.getSession();
 
         session.setAttribute("loginInfo",str);  // 구성한 문자열을 HttpSession의 loginInfo이름을 이용해서 저장한다.
 
-        resp.sendRedirect("todo/list"); // 로그인이 처리된 후에는 목록페이지로 리다이렉트
+        resp.sendRedirect("todo/list"); // 로그인이 처리된 후에는 목록페이지로 리다이렉트 */
+
+        try {   // 정상적으로 로그인 된 경우 Httpsession 을 이용해서 'loginInfo'이름으로 객체를 저장
+            MemberDTO memberDTO = MemberService.INSTANCE.login(mid, mpw);
+            HttpSession session = req.getSession();
+            session.setAttribute("loginInfo", memberDTO);
+            resp.sendRedirect("todo/list");
+        } catch (Exception e) { // 예외가 발생하는 경우 /login으로 이동(result 파라미터를 전달해서 문제발생을 같이 전달)
+            resp.sendRedirect("login?result=error");
+        }
     }
 }
